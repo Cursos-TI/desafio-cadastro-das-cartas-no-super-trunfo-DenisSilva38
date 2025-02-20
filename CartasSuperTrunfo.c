5849ad6c2d56f4b3d6d032327916ef73abf63324
DenisSilva38 <denissilvadeoliveira@yahoo.com.br>
2025-02-20 02:17:18 +0000

    CartasSuperTrunfo Final

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,6 +2,32 @@
 
 int main(){
     printf("Cartas Super Trunfo  \n"); 
+    printf("Carta 1  \n");
+
+    int pontosturisticos , populaçao;
+    float area , PIB;
+    char estado ['A'];
+    char nome[20]; 
+    char codigodacarta[20];
+    
+    printf("estado:A  \n");
+    printf("codigo: A01  \n");
+    printf("nome: Sao Paulo  \n");
+    printf("populaçao: 25.0000  \n");
+    printf("area: 1532.11 km²  \n");
+    printf("PIB: 32.00000 bilhoes de reais  \n");
+    printf("pontos turisticos: 50  \n");
+    printf("\n");
+    printf("Carta 2  \n");
+    printf("estado: B  \n");
+    printf("codigo: B02  \n");
+    printf("nome: Rio de Janeiro  \n");
+    printf("populaçao: 22.0000  \n");
+    printf("area: 115.09 km²  \n");
+    printf("PIB: 22.00000 bilhoes de reais  \n");
+    printf("pontos turisticos: 28  ");
+
+
 
 
     return 0;
